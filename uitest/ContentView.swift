474c5096c15bc14fe0c4f1897747db45f47de55b
Axel Hodler <axel@hodler.co>
2020-07-05 10:40:48 +0200

    sample tests

--- uitest/ContentView.swift
+++ uitest/ContentView.swift
@@ -1,8 +1,58 @@
 import SwiftUI
 
-struct ContentView: View {
+class Settings: ObservableObject {
+
+    private let userDefaults: UserDefaults
+
+    init(userDefaults: UserDefaults = UserDefaults.standard) {
+        self.userDefaults = userDefaults
+        self.showTutorial = !userDefaults.bool(forKey: "seenTutorial")
+    }
+
+    @Published var showTutorial: Bool {
+        didSet {
+            markTutorialAsShown()
+        }
+    }
+
+    private func markTutorialAsShown() {
+        userDefaults.set(true, forKey: "seenTutorial")
+    }
+}
+
+struct TutorialView: View {
+
+    @Binding var tutorialShown: Bool
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Tutorial")
+                .font(.largeTitle)
+            Spacer()
+            Button(action: {
+                self.tutorialShown = false
+            }, label: {
+                Text("Continue")
+                    .font(.largeTitle)
+            })
+        }
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var settings: Settings
+
+    init(settings: Settings = Settings()) {
+        self.settings = settings
+    }
+
+    var body: some View {
+        VStack {
+            Text("Main Content")
+                .font(.largeTitle)
+        }.sheet(isPresented: self.$settings.showTutorial, content: {
+            TutorialView(tutorialShown: self.$settings.showTutorial)
+        })
     }
 }
 
